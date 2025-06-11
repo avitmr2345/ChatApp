b64fba988dce00dabb869e3c27c3437d3dacaa0f
Avish <avitmr2345@gmail.com>
2025-06-11 21:44:48 +0530

    added auth routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 const port = 3000;
+
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
