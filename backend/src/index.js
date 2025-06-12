494a6f699da7fc873b3aca5939ca60c787860692
Avish <avitmr2345@gmail.com>
2025-06-12 11:48:26 +0530

    connected to mongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 
+dotenv.config();
+
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
