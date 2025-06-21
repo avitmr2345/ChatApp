94caf35712ff75193da8dce674a4059617dcdf2f
Avish <avitmr2345@gmail.com>
2025-06-21 10:11:06 +0530

    feat: created profile page with image upload

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
@@ -21,7 +21,7 @@ app.use(
 );
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
