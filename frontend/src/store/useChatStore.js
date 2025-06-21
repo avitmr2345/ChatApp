514acacd21888de5740313895b491a83bf44c1dd
Avish <avitmr2345@gmail.com>
2025-06-21 13:30:55 +0530

    feat: added Sidebar component for contacts and integrated users loading state

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,4 +1,6 @@
 import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios";
 
 export const useChatStore = create((set) => ({
   messages: [],
@@ -8,4 +10,17 @@ export const useChatStore = create((set) => ({
   isMessagesLoading: false,
   
   setSelectedUser: (selectedUser) => set({ selectedUser }),
+  
+  getUsers: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const res = await axiosInstance.get("/messages/users");
+      set({ users: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+
 }));
