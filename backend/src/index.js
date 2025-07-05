f16f3458f397ce931fd0bb4a9b343fd0f2a1c51e
Avish <avitmr2345@gmail.com>
2025-07-05 22:16:01 +0530

    feat: update CORS configuration to allow requests from Render deployment (not necessary because client and frontend are served from same domain, but it's good practice to include your production URL)

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://chatapp-7l3v.onrender.com"],
     credentials: true, // Allow credentials to be sent with requests like cookies or authorization headers
   })
 );
